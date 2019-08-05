2794f95a705ac1108fa990c0269e6cb416e5a66f
jkylecarpentry@gmail.com <jkylecarpentry@gmail.com>
2019-08-05 16:58:41 -0400

    Added Insert for Customer and Order

--- CSharp2SQLProject/Program.cs
+++ CSharp2SQLProject/Program.cs
@@ -6,6 +6,22 @@ namespace CSharp2SQLProject {
     class Program {
         static void Main(string[] args) {
 
+            var ord = GetOrderById(2001);
+            if(ord == null) {
+                Console.WriteLine("Customer not found");
+            }
+            else {
+                Console.WriteLine(ord.Id);
+            }
+
+            var cust = GetCustomerById(1028);
+            if(cust == null) {
+                Console.WriteLine("Customer not found");
+            }
+            else {
+                Console.WriteLine(cust.Name);
+            }
+
             var sql1 = "SELECT * from Orders;";
             var orders = SelectOrder(sql1);
             foreach (var order in orders) {
@@ -75,5 +91,61 @@ namespace CSharp2SQLProject {
             connection.Close();
             return customerList;
         }
-    }
+        static Customer GetCustomerById(int pid) {
+            var connStr = @"server=localhost\sqlexpress;database = CustomerOrderDb;trusted_connection=true;";
+            var connection = new SqlConnection(connStr);
+            connection.Open();
+            if (connection.State != System.Data.ConnectionState.Open) {
+                throw new Exception("Connection did not open!");
+            }
+            var sql = "SELECT * From Customers Where Id = @myid;";
+            var cmd = new SqlCommand(sql, connection);
+            var theId = new SqlParameter("@myid", pid);
+            cmd.Parameters.Add(theId);
+            var reader = cmd.ExecuteReader();
+            Customer cust = null;
+            if(reader.Read()) {
+                var id = (int)reader["Id"];
+                var name = reader["Name"].ToString();
+                var city = reader["City"].ToString();
+                var state = reader["State"].ToString();
+                var active = (bool)reader["Active"];
+                var code = reader.IsDBNull(reader.GetOrdinal("Code"))
+                    ? null
+                    : reader["Code"].ToString();
+                cust = new Customer(id, name, city, state, active, code);
+            }
+            reader.Close();
+            connection.Close();
+
+            return cust;
+        }
+        static Order GetOrderById(int oid) {
+            var connStr = @"server=localhost\sqlexpress;database = CustomerOrderDb;trusted_connection=true;";
+            var connection = new SqlConnection(connStr);
+            connection.Open();
+            if (connection.State != System.Data.ConnectionState.Open) {
+                throw new Exception("Connection did not open!");
+            }
+            var sql = "Select * from Orders Where Id = @myorderid;";
+            var cmd = new SqlCommand(sql, connection);
+            var theId = new SqlParameter("@myorderid", oid);
+            cmd.Parameters.Add(theId);
+            var reader = cmd.ExecuteReader();
+            Order ord = null;
+            if (reader.Read()) {
+                var id = (int)reader["Id"];
+                var date = (DateTime)reader["Date"];
+                var note = reader["Note"].ToString();
+                var customerId = reader["CustomerId"].ToString();
+                
+                ord = new Order(id, date, note, customerId);
+            }
+            reader.Close();
+            connection.Close();
+
+            return ord;
+        }
+
+}
 }
